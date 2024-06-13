1822c666f7d610ed754a1dce83915ec05922f4fb
paulogabrielv <steampaulogabriel@hotmail.com>
2024-06-13 19:50:43 -0300

    Meu primeiro commit do site do projeto

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja Paulo</title>
+    <link rel="stylesheet" href="./css/style.css">    
+</head>
+<body>
+
+     <div class="banner">
+
+       <div class="container">
+
+   
+         <div class="navbar">
+              
+          <div class="logo">
+            <img src="./img/logo.png.jpg" alt="PauloGames">
+          </div>
+
+          <nav>
+            <ul id="Menu">
+              <li><a href="" title="">Inicio</a></li>
+              <li><a href="" title="">Lançamentos</a></li>
+              <li><a href="" title="">Promoção</a></li>
+              <li><a href="" title="">Forum</a></li>
+              <li><a href="" title="">Contato</a></li>
+            </ul>
+          </nav>
+
+         </div>
+
+
+       </div>
+
+     </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <scripts src="./js/app.js"></scripts>
+</body>
+</html>
