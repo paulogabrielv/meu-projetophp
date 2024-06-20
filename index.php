12af1d28ff4d922321e6367178de69584f8d879c
paulogabrielv <steampaulogabriel@hotmail.com>
2024-06-20 20:16:29 -0300

    Meu segundo commit

--- index.php
+++ index.php
@@ -1,43 +1,100 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loja Paulo</title>
-    <link rel="stylesheet" href="./css/style.css">    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Paulo Pagina</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <script src="script.js" defer></script>
+   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 <body>
 
-     <div class="banner">
-
-       <div class="container">
-
-   
-         <div class="navbar">
-              
-          <div class="logo">
-            <img src="./img/logo.png.jpg" alt="PauloGames">
-          </div>
-
-          <nav>
-            <ul id="Menu">
-              <li><a href="" title="">Inicio</a></li>
-              <li><a href="" title="">Lançamentos</a></li>
-              <li><a href="" title="">Promoção</a></li>
-              <li><a href="" title="">Forum</a></li>
-              <li><a href="" title="">Contato</a></li>
-            </ul>
-          </nav>
-
-         </div>
 
 
-       </div>
+  <header class="header">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        
+        <a class="navbar-brand" href="index.php?pg=home">
+        <img class="navbarimg" src="imagens/1logov2.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-     </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php?pg=home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php?pg=jogos">Jogos</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                Opções
+              </a>      
+        <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="index.php?pg=contato">Contato</a></li>
+                <li><a class="dropdown-item" href="index.php?pg=equipe">Equipe</a></li>
+                <li>
+                  
+              </ul>
 
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <scripts src="./js/app.js"></scripts>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+
+
+  </header>
+  <main>
+    <?php
+    
+    
+    $pg = $_GET["pg"] ?? "home";
+    $pg = "paginas/{$pg}.php";
+    if (file_exists($pg)) {
+      include $pg;
+    } else {
+      include "paginas/erro.php";
+    }
+
+    ?>
+  </main>
+  <footer class="footer">
+    <p>Todos os direitos reservados | Desenvolvido por Paulo Vendramini</p>
+    
+    <a href="https://wa.me/5544999968386">
+    <ion-icon name="logo-whatsapp" size="large"></ion-icon>
+  </a>
+  <a href="">
+    <ion-icon name="logo-tiktok" size="large"></ion-icon>
+  </a>
+  <a href="">
+    <ion-icon name="logo-discord" size="large"></ion-icon>
+  </a>
+    <a href="https://github.com/LGazola">
+    <ion-icon name="logo-github" size="large"></ion-icon>
+  </a>
+  <a href="https://www.instagram.com/gazola_luizz?igsh=aHQ4bmpoNDY5M3Ry">
+    <ion-icon name="logo-instagram" size="large"></ion-icon>
+</a>
+
+  </footer>
+
 </body>
+
 </html>
---
+++ paginas/home.php
@@ -0,0 +1,58 @@
+<main>
+
+     <div class="carrosel" id="carrossel">
+        <div class="container" id="img">
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/bannerjogo.png" alt="Space Soldier">
+            </a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/fase1.jpeg" alt="LeMinotaur"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/headerlogo1.png" alt="Space Soldier"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/fase2.jpeg" alt="LeMinotaur"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/courtofkingcrimson.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/deathconsciousness.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/larks.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/lysflath.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/madvillany.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/MrMorale.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/ncudaod.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/okcomputer.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/thebends.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/tomjobim.jpeg" alt="Álbum"></a>
+            <a href="https://open.spotify.com/user/22lkbi57gdgwd6j45sa7ncmla?si=sznv6mxASgeJ6gSc_GYfRQ" target="blank">
+                <img src="imagens/ye.jpeg" alt="Álbum"></a>
+
+        </div>
+    </div>
+    <div class="content">
+    <div class="card-h">
+    <div class="card text-center">
+  <div class="card-header">
+    Jogos
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Descrição dos jogos</h5>
+    <p class="card-text">saiba mais dos jogos antes de jogar, suas histórias, controles, mecânicas, etc.</p>
+    
+    <a href="index.php?pg=jogos" class="btn btn-primary">Jogos</a>
+
+  </div>
+  <div class="card-footer text-body-secondary">
+    </div>
+  </div>
+</div>
+    </div>
+    
+</main>
